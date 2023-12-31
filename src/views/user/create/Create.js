e8b9882c2de448e83988595271f88e5a552c1c47
juan sebastian <juan.quiroz@gse.com.co>
2023-12-31 06:45:55 -0500

    changes validate user

--- src/views/user/create/Create.js
+++ src/views/user/create/Create.js
@@ -30,7 +30,20 @@ function Create({ setView,getAll }) {
   const [inputVin, setInputVin] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
-  const [error, setError] = useState('');
+  const [errorUser, setErrorUser] = useState('');
+  const [errorDni, setErrorDni] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorSurname, setErrorSurname] = useState('');
+  const [errorPhone, setErrorPhone] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+  const [errorVin, setErrorVin] = useState('');
+  const [errorRole, setErrorRole] = useState('');
+  const [errorStatus, setErrorStatus] = useState('');
+  const [errorBrand, setErrorBrand] = useState('');
+  const [errorModel, setErrorModel] = useState('');
+  const [errorType, setErrorType] = useState('');
+  const [errorShow, setErrorShow] = useState('');
   const [opcionRole, setOpcionRole] = useState([]);
   const [opcionStatus, setOpcionStatus] = useState([]);
   const [opcionShow, setOpcionShow] = useState([]);
@@ -81,6 +94,7 @@ function Create({ setView,getAll }) {
   }
 
   const resetInput = () => {
+    //Este formatea los inputs
     setInputUser("")
     setInputDni("")
     setInputName("")
@@ -89,11 +103,29 @@ function Create({ setView,getAll }) {
     setInputEmail("")
     setInputPassword("")
     setInputVin("")
+
+    //Este formatea los select
     setOpcionSelectStatus("")
     setOpcionSelectBrand("")
     setOpcionSelectModel("")
     setOpcionSelectType("")
     setOpcionSelectShow("")
+
+    //Este formatea los mensajes de errores
+    setErrorUser("")
+    setErrorVin("")
+    setErrorDni("")
+    setErrorName("")
+    setErrorSurname("")
+    setErrorPhone("")
+    setErrorEmail("")
+    setErrorPassword("")
+    setErrorRole("")
+    setErrorStatus("")
+    setErrorBrand("")
+    setErrorModel("")
+    setErrorType("")
+    setErrorShow("")
   }
 
   const handleChangeRole = (e) => {
@@ -101,72 +133,231 @@ function Create({ setView,getAll }) {
     setOpcionSelectRole(e.target.value);
   }
 
-  // const create = () => {
-  //   console.log(opcionSelectRole);
-  //   console.log(opcionSelectStatus);
-  //   console.log(opcionSelectBrand);
-  //   console.log(opcionSelectModel);
-  //   console.log(opcionSelectType);
-  //   console.log(opcionSelectShow);
-  // }
-
-  const validateInput = () => {
-    let message = true;
-    // Validaciones
-    if (inputName.trim() === '') {
-      setError('El campo no puede estar en blanco.');
-      message = false;
-      return message
-    } else if (inputName.includes(' ')) {
-      setError('El campo no puede contener espacios en blanco.');
-      message = false;
-      return message 
-    } else if (inputName.length < 4) {
-      setError(`El campo debe tener al menos ${4} caracteres.`);
-      message = false;
-      return message
-    } else {
-      setError('');
-      message = true;
-      return message
+  const handleChangeDni = (e) => {
+    // valida que solo se escriban numeros
+    const esValido = e.target.validity.valid;
+    if (esValido) {
+      setInputDni(e.target.value);
     }
+  } 
+
+  const handleChangePhone = (e) => {
+    // valida que solo se escriban numeros
+    const esValido = e.target.validity.valid;
+    if (esValido) {
+      setInputPhone(e.target.value);
+    }
+  } 
+
+  const validateField = (value, fieldName, regex, minLength, customErrorMessage) => {
+    if (value.trim() === '') {
+      return `El campo ${fieldName} no puede estar en blanco.`;
+    }
+
+    if (regex && !regex.test(value)) {
+      return customErrorMessage || `El campo ${fieldName} no cumple con el formato esperado.`;
+    }
+
+    if (value.length < minLength) {
+      return `El campo ${fieldName} debe tener al menos ${minLength} caracteres.`;
+    }
+
+    return null; // Indica que la validación fue exitosa
+  };
+
+  const validateVehicle = () => {
+    let isValid = true;
+
+    const userError = validateField(inputUser, 'user', /^[a-zA-Z0-9_]+$/, 4);
+    if (userError) {
+      setErrorUser(userError);
+      isValid = false;
+    } else {
+      setErrorUser("");
+    }
+
+    const vinError = validateField(inputVin, 'vin', /^[a-zA-Z0-9_]+$/, 4);
+    if (vinError) {
+      setErrorVin(vinError);
+      isValid = false;
+    } else {
+      setErrorVin("");
+    }
+
+    const emailError = validateField(inputEmail, 'email', /^[a-zA-Z0-9_]+$/, 4);
+    if (emailError) {
+      setErrorEmail(emailError);
+      isValid = false;
+    } else {
+      setErrorEmail("");
+    }
+
+    const brandError = validateField(opcionSelectBrand, 'brand', /\S/, 1);
+    if (brandError) {
+      setErrorBrand(brandError);
+      isValid = false;
+    } else {
+      setErrorBrand("");
+    }
+
+    const modelError = validateField(opcionSelectModel, 'model', /\S/, 1);
+    if (modelError) {
+      setErrorModel(modelError);
+      isValid = false;
+    } else {
+      setErrorModel("");
+    }
+
+    const typeError = validateField(opcionSelectType, 'type', /\S/, 1);
+    if (typeError) {
+      setErrorType(typeError);
+      isValid = false;
+    } else {
+      setErrorType("");
+    }
+
+    const showError = validateField(opcionSelectShow, 'show', /\S/, 1);
+    if (showError) {
+      setErrorShow(showError);
+      isValid = false;
+    } else {
+      setErrorShow("");
+    }
+
+    const passwordError = validateField(inputPassword, 'password', /^[a-zA-Z0-9_]+$/, 4);
+    if (passwordError) {
+      setErrorPassword(passwordError);
+      isValid = false;
+    } else {
+      setErrorPassword("");
+    }
+
+    // Puedes agregar más bloques de validaciones para otros campos si es necesario
+    return isValid;
+
   }
 
+  const validateDriver = () => {
+    let isValid = true;
+
+    const userError = validateField(inputUser, 'user', /^[a-zA-Z0-9_]+$/, 4);
+    if (userError) {
+      setErrorUser(userError);
+      isValid = false;
+    } else {
+      setErrorUser("");
+    }
+
+    const dniError = validateField(inputDni, 'dni', /^[0-9]+$/, 4);
+    if (dniError) {
+      setErrorDni(dniError);
+      isValid = false;
+    } else {
+      setErrorDni("");
+    }
+
+    const nameError = validateField(inputName, 'name', /^[a-zA-Z0-9_]+$/, 4);
+    if (nameError) {
+      setErrorName(nameError);
+      isValid = false;
+    } else {
+      setErrorName("");
+    }
+
+    const surnameError = validateField(inputSurname, 'surname', /^[a-zA-Z0-9_]+$/, 4);
+    if (surnameError) {
+      setErrorSurname(surnameError);
+      isValid = false;
+    } else {
+      setErrorSurname("");
+    }
+
+    const phoneError = validateField(inputPhone, 'phone', /^[0-9]+$/, 4);
+    if (phoneError) {
+      setErrorPhone(phoneError);
+      isValid = false;
+    } else {
+      setErrorPhone("");
+    }
+
+    const emailError = validateField(inputEmail, 'email', /^[a-zA-Z0-9_]+$/, 4);
+    if (emailError) {
+      setErrorEmail(emailError);
+      isValid = false;
+    } else {
+      setErrorEmail("");
+    }
+
+    const roleError = validateField(opcionSelectRole, 'role', /\S/, 1);
+    if (roleError) {
+      setErrorRole(roleError);
+      isValid = false;
+    } else {
+      setErrorRole("");
+    }
+
+    const statusError = validateField(opcionSelectStatus, 'status', /\S/, 1);
+    if (statusError) {
+      setErrorStatus(statusError);
+      isValid = false;
+    } else {
+      setErrorStatus("");
+    }
+
+    const showError = validateField(opcionSelectShow, 'show', /\S/, 1);
+    if (showError) {
+      setErrorShow(showError);
+      isValid = false;
+    } else {
+      setErrorShow("");
+    }
+
+    const passwordError = validateField(inputPassword, 'password', /^[a-zA-Z0-9_]+$/, 4);
+    if (passwordError) {
+      setErrorPassword(passwordError);
+      isValid = false;
+    } else {
+      setErrorPassword("");
+    }
+
+    // Puedes agregar más bloques de validaciones para otros campos si es necesario
+    return isValid;
+  };
+
   const create = async () => {
-    //Aqui estoy validando que el input cumpla con las validaciones
-    let validate = validateInput();
-    if(validate === true) {
+
+    const isValid = validateDriver();
+    console.log(isValid);
+    if (isValid) {
       //Aquí comienza las peticiones y demas
-      let body = { name:inputName }
-      let response = await dispatch(createUserService(body));
-      if(response.error === undefined){
-        switch (response.response.status) {
-          case 201:
-              //Aquí estoy limpiando el input
-              setInputName("")
-              Swal.fire({
-                title: "Creado!",
-                text: "Fue creado con exito",
-                icon: "success"
-              });
-            break;
-          default:
-              console.log(response.response);
-              Swal.fire({
-                title: "Error!",
-                text: "Ocurrio un error al crearlo",
-                icon: "error"
-              });
-            break;
-        }
-      } else {
-        console.log(response.error);
-        Swal.fire({
-          title: "Error!",
-          text: "Eror al crear el usuario",
-          icon: "error"
-        });
-      }
+      // let body = { name:inputName }
+      // let response = await dispatch(createUserService(body));
+      // if(response.error === undefined){
+      //   switch (response.response.status) {
+      //     case 201:
+      //         Swal.fire({
+      //           title: "Creado!",
+      //           text: "Fue creado con exito",
+      //           icon: "success"
+      //         });
+      //       break;
+      //     default:
+      //         console.log(response.response);
+      //         Swal.fire({
+      //           title: "Error!",
+      //           text: "Ocurrio un error al crearlo",
+      //           icon: "error"
+      //         });
+      //       break;
+      //   }
+      // } else {
+      //   console.log(response.error);
+      //   Swal.fire({
+      //     title: "Error!",
+      //     text: "Eror al crear el usuario",
+      //     icon: "error"
+      //   });
+      // }
     }
   }
 
@@ -185,28 +376,52 @@ function Create({ setView,getAll }) {
                 <input value={inputUser} onChange={(e) => setInputUser(e.target.value)} type="text" className="user-create-input form-control" placeholder="Usuario" />
               </div>
 
+              <div className='mt-4'>
+                {errorUser && <p style={{ color: 'red' }}>{errorUser}</p>}
+              </div>
+
               { opcionSelectRole === "Administrador" || opcionSelectRole === "Conductor" ? 
                 <>
                   <div className='mt-4 user-create-main-input'>
-                      <input value={inputDni} onChange={(e) => setInputDni(e.target.value)} type="text" className="user-create-input form-control" placeholder="Cedula" />
+                      <input value={inputDni} onChange={handleChangeDni} pattern="[0-9]{0,13}" type="text" className="user-create-input form-control" placeholder="Cedula" />
+                  </div>
+
+                  <div className='mt-4'>
+                    {errorDni && <p style={{ color: 'red' }}>{errorDni}</p>}
                   </div>
 
                   <div className='mt-4 user-create-main-input'>
                     <input value={inputName} onChange={(e) => setInputName(e.target.value)} type="text" className="user-create-input form-control" placeholder="Nombres" />
                   </div>
 
+                  <div className='mt-4'>
+                    {errorName && <p style={{ color: 'red' }}>{errorName}</p>}
+                  </div>
+
                   <div className='mt-4 user-create-main-input'>
                     <input value={inputSurname} onChange={(e) => setInputSurname(e.target.value)} type="text" className="user-create-input form-control" placeholder="Apellidos" />
                   </div>
 
+                  <div className='mt-4'>
+                    {errorSurname && <p style={{ color: 'red' }}>{errorSurname}</p>}
+                  </div>
+
                   <div className='mt-4 user-create-main-input'>
-                    <input value={inputPhone} onChange={(e) => setInputPhone(e.target.value)} type="text" className="user-create-input form-control" placeholder="Telefono" />
+                    <input value={inputPhone} onChange={handleChangePhone} pattern="[0-9]{0,13}" type="text" className="user-create-input form-control" placeholder="Telefono" />
+                  </div>
+
+                  <div className='mt-4'>
+                    {errorPhone && <p style={{ color: 'red' }}>{errorPhone}</p>}
                   </div>
                 </> 
                 : opcionSelectRole === "Vehiculo" || opcionSelectRole === "Asociado" ? 
                 <>
                   <div className='mt-4 user-create-main-input'>
                       <input value={inputVin} onChange={(e) => setInputVin(e.target.value)} type="text" className="user-create-input form-control" placeholder="Vin" />
+                  </div>
+
+                  <div className='mt-4'>
+                    {errorVin && <p style={{ color: 'red' }}>{errorVin}</p>}
                   </div>
 
                   <div className='mt-4 user-create-main-input form-group'>
@@ -220,6 +435,10 @@ function Create({ setView,getAll }) {
                     </select>
                   </div>
 
+                  <div className='mt-4'>
+                    {errorBrand && <p style={{ color: 'red' }}>{errorBrand}</p>}
+                  </div>
+
                   <div className='mt-4 user-create-main-input form-group'>
                     <select value={opcionSelectModel} onChange={(e) => setOpcionSelectModel(e.target.value)} className='user-create-input form-control'>
                       <option value="">Selecciona una opción - modelo</option>
@@ -229,6 +448,10 @@ function Create({ setView,getAll }) {
                         </option>
                       ))}
                     </select>
+                  </div>
+
+                  <div className='mt-4'>
+                    {errorModel && <p style={{ color: 'red' }}>{errorModel}</p>}
                   </div>
 
                   <div className='mt-4 user-create-main-input form-group'>
@@ -241,12 +464,21 @@ function Create({ setView,getAll }) {
                       ))}
                     </select>
                   </div>
+
+                  <div className='mt-4'>
+                    {errorType && <p style={{ color: 'red' }}>{errorType}</p>}
+                  </div>
+
                 </> 
                 : <></>
               }
 
               <div className='mt-4 user-create-main-input'>
                 <input value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} type="email" className="user-create-input form-control" placeholder="Correo" />
+              </div>
+
+              <div className='mt-4'>
+                {errorEmail && <p style={{ color: 'red' }}>{errorEmail}</p>}
               </div>
 
               <div className='mt-4 user-create-main-input form-group'>
@@ -260,6 +492,10 @@ function Create({ setView,getAll }) {
                 </select>
               </div>
 
+              <div className='mt-4'>
+                {errorRole && <p style={{ color: 'red' }}>{errorRole}</p>}
+              </div>
+
               <div className='mt-4 user-create-main-input form-group'>
                 <select value={opcionSelectStatus} onChange={(e) => setOpcionSelectStatus(e.target.value)} className='user-create-input form-control'>
                   <option value="">Selecciona una opción - estado</option>
@@ -269,6 +505,10 @@ function Create({ setView,getAll }) {
                     </option>
                   ))}
                 </select>
+              </div>
+
+              <div className='mt-4'>
+                {errorStatus && <p style={{ color: 'red' }}>{errorStatus}</p>}
               </div>
 
               <div className='mt-4 user-create-main-input form-group'>
@@ -282,12 +522,16 @@ function Create({ setView,getAll }) {
                 </select>
               </div>
 
+              <div className='mt-4'>
+                {errorShow && <p style={{ color: 'red' }}>{errorShow}</p>}
+              </div>
+
               <div className='mt-4 user-create-main-input'>
                 <input value={inputPassword} onChange={(e) => setInputPassword(e.target.value)} type="text" className="user-create-input form-control" placeholder="Contraseña" />
               </div>
               
               <div className='mt-4'>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
+                {errorPassword && <p style={{ color: 'red' }}>{errorPassword}</p>}
               </div>
 
               <div className='mt-4 text-center'>
